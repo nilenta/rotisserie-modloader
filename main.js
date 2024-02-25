ccef31234c8b936ac84818c032e9426ec12785f5
nilenta <145288586+nilenta@users.noreply.github.com>
2024-02-25 12:06:06 -0600

    cookie

--- main.js
+++ main.js
@@ -41,6 +41,34 @@ const rotisserie = {
                 parentElement.remove();
             });
         })
+    },
+    cookie: {
+        set: function(name, value, days = 30) {
+            var expires = "";
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                expires = "; expires=" + date.toUTCString();
+            }
+            document.cookie = name + "=" + value + expires + "; path=/";
+        },
+        get: function(name) {
+            var nameEQ = name + "=";
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i];
+                while (cookie.charAt(0) == ' ') {
+                    cookie = cookie.substring(1, cookie.length);
+                }
+                if (cookie.indexOf(nameEQ) == 0) {
+                    return cookie.substring(nameEQ.length, cookie.length);
+                }
+            }
+            return null;
+        },
+        delete: function(name) {
+            document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        }
     }
 }
 
