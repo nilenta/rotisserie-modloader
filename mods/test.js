c753f54aff71596b6e0f02f306fdd064b99074e9
nilenta <145288586+nilenta@users.noreply.github.com>
2024-02-24 23:47:30 -0600

    Create test.js

---
+++ mods/test.js
@@ -0,0 +1,17 @@
+ext_mods.push({
+    info: {
+        name: 'EXTERNAL MOD TESTING',
+        description: `Test for external mods.`,
+        version: `1.0.0`,
+        creator: 'natsu',
+        class_to_look_for: `message`,
+        does_look_for_classes: true,
+        custom_message: false
+    },
+    startup: function() {
+
+    },
+    callback: (node) => {
+
+    }
+})
